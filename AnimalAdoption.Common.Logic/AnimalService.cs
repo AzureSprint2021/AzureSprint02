922e35ce73030764687ee95a679a3a48b68e9de4
softnareum <areum.han@softn.kr>
2021-05-25 14:16:48 +0900

    팀 구성 수정

--- AnimalAdoption.Common.Logic/AnimalService.cs
+++ AnimalAdoption.Common.Logic/AnimalService.cs
@@ -7,9 +7,9 @@ namespace AnimalAdoption.Common.Logic
     public class AnimalService
     {
         public Animal[] ListAnimals => new Animal[] {
-            new Animal { Id = 1, Name = "Sedi", Age = 1, Description = "Soft natured" },
-            new Animal { Id = 2, Name = "Metamorph", Age = 50, Description = "Under a lot of pressure" },
-            new Animal { Id = 3, Name = "Igno", Age = 50, Description = "Shiny and glasslike" },
+            new Animal { Id = 1, Name = "Yeonil Jeong", Age = 30, Description = "차장" },
+            new Animal { Id = 2, Name = "Areum Han", Age = 29, Description = "대리" },
+            new Animal { Id = 3, Name = "Jieun Kim", Age = 28, Description = "사원" }
         };
     }
 }
